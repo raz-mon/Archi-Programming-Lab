e57a3beba1451a8dfc4aae2ca06f746ecb188921
almog <almogze7@gmail.com>
2021-04-23 00:25:35 +0300

    task0a

--- Labs/Lab5/task 0a/main.c
+++ Labs/Lab5/task 0a/main.c
@@ -1,8 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "LineParser.h"
 
-
-
+void execute(cmdLine * pCmdLine);
 
 int main(int argc, char ** argv){
 
+    char * input;
+    fgets(input,2048,stdin);
+    cmdLine * CL = parseCmdLines(input);
+    execute(CL);
+
     return 0;
+}
+
+void execute(cmdLine * pCmdLine){
+    char buff[PATH_MAX];
+    getcwd(buff,PATH_MAX);
+    printf("%s\n" , buff);
+
+    int test = execv(buff, pCmdLine->arguments);
+    printf("%d" , test);
 }
