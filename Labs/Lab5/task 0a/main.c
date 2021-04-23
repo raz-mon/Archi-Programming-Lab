8de26b18d03578b91c11fef737ab3167782faabb
almog <almogze7@gmail.com>
2021-04-23 18:50:03 +0300

    dizziy

--- Labs/Lab5/task 0a/main.c
+++ Labs/Lab5/task 0a/main.c
@@ -6,21 +6,41 @@
 
 void execute(cmdLine * pCmdLine);
 
-int main(int argc, char ** argv){
-
-    char * input;
-    fgets(input,2048,stdin);
-    cmdLine * CL = parseCmdLines(input);
-    execute(CL);
-
-    return 0;
-}
-
-void execute(cmdLine * pCmdLine){
+int main(int argc, char * argv[]){
+    /*
     char buff[PATH_MAX];
     getcwd(buff,PATH_MAX);
     printf("%s\n" , buff);
+    */
+    
+    char * input;
+    fgets(input,2048,stdin);
+    //cmdLine * CL = parseCmdLines(input);
+    //printf("Number of arguments: %d\n" , CL->argCount);
 
-    int test = execv(buff, pCmdLine->arguments);
-    printf("%d" , test);
+    printf("Arguments: ");
+    //for(int i =0; i < CL->argCount ; i++){
+    //    printf(" %s" , CL->arguments[i]);
+    //}
+    printf("\n");
+    //execute(CL);
+    
+    char * test2[] = {"./Test111" , NULL };
+    //execv("hello" , CL->arguments);
+    //perror("Fault");
+    return 0;
 }
+
+
+
+/*
+void execute(cmdLine * pCmdLine){
+    FILE * f = fopen("Test111" , "w+");
+    //fclose(f);
+    char * test2[] = {"./Test111" , (char *)0 };
+    //int test = 
+    execv("./Test111", test2);
+    perror("Fault");
+    //printf("%d" , test);
+}
+*/
