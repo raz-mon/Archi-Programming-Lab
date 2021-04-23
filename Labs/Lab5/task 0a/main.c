fb2319b274077a26d25fb43ba5e395f746eebc5d
almog <almogze7@gmail.com>
2021-04-23 19:48:10 +0300

    0a

--- Labs/Lab5/task 0a/main.c
+++ Labs/Lab5/task 0a/main.c
@@ -13,34 +13,18 @@ int main(int argc, char * argv[]){
     printf("%s\n" , buff);
     */
     
-    char * input;
+    char input[2048];
     fgets(input,2048,stdin);
-    //cmdLine * CL = parseCmdLines(input);
-    //printf("Number of arguments: %d\n" , CL->argCount);
+    cmdLine * CL = parseCmdLines(input);
+    execute(CL);
 
-    printf("Arguments: ");
-    //for(int i =0; i < CL->argCount ; i++){
-    //    printf(" %s" , CL->arguments[i]);
-    //}
-    printf("\n");
-    //execute(CL);
-    
-    char * test2[] = {"./Test111" , NULL };
-    //execv("hello" , CL->arguments);
-    //perror("Fault");
     return 0;
 }
 
 
 
-/*
+
 void execute(cmdLine * pCmdLine){
-    FILE * f = fopen("Test111" , "w+");
-    //fclose(f);
-    char * test2[] = {"./Test111" , (char *)0 };
-    //int test = 
-    execv("./Test111", test2);
+    execv(pCmdLine->arguments[0] , pCmdLine->arguments);
     perror("Fault");
-    //printf("%d" , test);
 }
-*/
