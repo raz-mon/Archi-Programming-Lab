6324e3a60ca83ce3d785adf888fbacf3498fca40
almog <almogze7@gmail.com>
2021-04-24 22:24:33 +0300

    task 0a

--- Labs/Lab5/task 0a/main.c
+++ Labs/Lab5/task 0a/main.c
@@ -3,21 +3,23 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "LineParser.h"
+#include <string.h>
 
 void execute(cmdLine * pCmdLine);
+char * fullDes(char * arg);
 
 int main(int argc, char * argv[]){
-    /*
-    char buff[PATH_MAX];
-    getcwd(buff,PATH_MAX);
-    printf("%s\n" , buff);
-    */
-    
-    char input[2048];
-    fgets(input,2048,stdin);
-    cmdLine * CL = parseCmdLines(input);
-    execute(CL);
-
+    while(1){
+        char input[2048];
+        fgets(input,2048,stdin);
+        cmdLine * CL = parseCmdLines(input);
+        if(CL->argCount ==1 && strcmp("quit" , CL->arguments[0]) == 0){
+            freeCmdLines(CL);
+            break;
+        }
+        execute(CL);
+        freeCmdLines(CL);
+    }
     return 0;
 }
 
@@ -25,6 +27,21 @@ int main(int argc, char * argv[]){
 
 
 void execute(cmdLine * pCmdLine){
-    execv(pCmdLine->arguments[0] , pCmdLine->arguments);
-    perror("Fault");
+    char * fullName = fullDes(pCmdLine->arguments[0]);
+    if(fork() == 0){
+        if(execvp(fullName , pCmdLine->arguments) < 0){
+            perror("Fault");
+            free(fullName);
+            exit(EXIT_FAILURE);
+        }
+    }
+    free(fullName);
 }
+
+char * fullDes(char * arg){
+    char * buff = (char*)(malloc(PATH_MAX*sizeof(char)));
+    getcwd(buff,PATH_MAX);
+    strcat(buff,"/");
+    strcat(buff,arg);
+    return buff;
+} 
