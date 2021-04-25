f11045ef7e9d9576a820d4882ba6f655edd3f02d
razmon <razmon@post.bgu.ac.il>
2021-04-25 19:27:07 +0300

    update

--- Labs/Lab5/task1a/main.c
+++ Labs/Lab5/task1a/main.c
@@ -5,12 +5,15 @@
 #include <unistd.h>
 #include <string.h>
 
+int debug_mode = 0;
+
 void execute(cmdLine* pCmdLine){
     char path[PATH_MAX];
     getcwd(path, PATH_MAX);
     char* real_path = strcat(path, "/");
     strcat(path, pCmdLine->arguments[0]);
-    if (fork() == 0){
+    int i = fork();
+    if (i == 0){
         int a = execvp(real_path, pCmdLine->arguments);
         if (a < 0 ){
             freeCmdLines(pCmdLine);
@@ -18,8 +21,18 @@ void execute(cmdLine* pCmdLine){
             exit(1);
         }
     }
+    else if (debug_mode == 1){
+        fprintf(stderr, "PID: %d\n", i);
+        fprintf(stderr, "Excecuting command: ");
+        for (int i = 0; i < pCmdLine->argCount; ++i){
+            fprintf(stderr, "%s", pCmdLine->arguments[i]);    
+        }
+        fprintf(stderr, "\n");
+    }
     freeCmdLines(pCmdLine);
 }
+
+
 
 int main(int argc, char* argv[]){
     
@@ -39,6 +52,10 @@ int main(int argc, char* argv[]){
     cmdLine* pCmdLine = parseCmdLines(line);
     if (strcmp(pCmdLine->arguments[0], "quit") == 0)
         exit(0);
+
+    if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'd')
+        debug_mode = 1;        // Debugger mode.
+    
     execute(pCmdLine);
 
     }
