e6b73eec1ce7d7b179080d7ca6de436584059787
razmon <razmon@post.bgu.ac.il>
2021-05-05 21:28:18 +0300

    sdfg

--- Assignments/Ass_2/calc.s
+++ Assignments/Ass_2/calc.s
@@ -32,12 +32,36 @@ section	.text
 	add	esp, STK_UNIT*1
 %endmacro
 
+%macro fgets_ass 0
+    push dword [stdin]              ;path to file(stdin)
+    push dword 81                   ;max lenght
+    push dword buffer               ;input buffer
+    call fgets
+    add esp, STK_UNIT*3                     
+%endmacro
+
+%macro create_new_link 0
+    push 5
+    call malloc
+    add esp, STK_UNIT * 1
+%endmacro
+
+%macro update_linkedlist 0
+    mov byte [eax], dl                              ; insert dl byte in the first byte in new link
+    mov esi, dword [current_link_ptr]               ; esi is now a ptr to the latest updated link (with offset 1)
+    mov dword [esi], eax                            ; old link point to the begginig of new link
+    inc esi                                         ; old link point to the second byte in new link 
+    mov dword [current_link_ptr], esi               ; current_link_ptr point to second byte in new link
+%endmacro
+
 
 
 section .bss                ; Uninitialized data.
     operand_stack: resd 63                              ; Remember to save what is the size is from the user.
+    buffer:        resb 81                              ; max size - input line , 80 bytes + 1 byte
+
 section .data               ; Initialized data.
-    
+    current_link_ptr: db 4
 
 section .rodata             ; Read-only data.
     initial_print: db "calc: ",10,0
@@ -111,7 +135,7 @@ mov dword[counter], 2
 mov edx, 0
 
 bit_loop:    
-    mov bx, 0
+    ;mov bx, 0
     mov esi, dword [pointer]
     mov bl, byte [esi]         ; bl points to the current character in the input.
     sub bl, 48                 ; get number-value of the input char (binary representation).
@@ -129,7 +153,10 @@ bit_loop:
     cmp cl, 8                  ; Check if we have 8 bits already.
     jl bit_loop                ; If cl<8 -> do the loop again.
 
-
+;construct_new_link: 
+;    create_new_link
+;    update_linkedlist
+;    jmp bit_loop
 
 print_dx_test:
     ;and dx, 0xFF
@@ -138,112 +165,38 @@ print_dx_test:
     call printf
     add esp, 8
 
-
-
-
-    mov esp, ebp                ; "release" the activation frame.
-    pop ebp                     ; restore activation frame of main.
-    ret                         ; return from the function.
-    ;endFunc                     ; Macro code will replace with code for exiting a function.
+    endFunc                     ; Macro code will replace with code for exiting a function.                       ; return from the function.
 
 
 end_of_program:                            ; End the program.
 
-;
-    
 
 
 
 
 
 
+;loop:
+;    fgets_ass                               ; stdio function fgets, put in buffer the wanted data
+;    mov edx, buffer                         ; pointer to buffer
+;    mov eax, 0                              
+;    mov ebx, 0                              ; counter for byte index
+;    mov al, byte [buffer + ebx]             ; set al as first buffer's byte
+;    cmp al, 10                              ; check if empty(only NL was sent)
+;    jz loop
 
-
-;        mov dl, byte [pointer_to_number_string]
-;    sub dl, 48
-;    mov byte[first_char], dl
-;    mov edx, pointer_to_number_string
-;    add edx, 1                      ; bl will point to the end of the input number. 
-;
-;    bit_loop:            
-;        mov bl, byte [edx]         ; bl points to the current character in the input.
-;        sub bl, 48                 ; get number-value of the input char (binary representation).
-;
-;        push bx
-;        push temp2
-;        call printf
-;        add esp, 6
-;
-;        shl bl, cl                 ; Put the bits in the right place before adding to ax.
-;        add cl, 3
-;        add ax, bx                 ; Add bits to the representation.
-;
-;        dec edx
-;        
-;        cmp bl, [first_char]        ; Check if there are any numbers left to read in the input.
-;        jz end_of_program           ; If not, finish the loop. 
-;
-;        cmp cl, 8                  ; Check if we have 8 bits already.
-;        jl bit_loop                ; If cl<8 -> do the loop again.
-
-
-
-
-
-
-
-
-
-
-
-    ; Old code.
-    ;allocate the operand-stack (default size)
-    ;push dword 20
-    ;call malloc
-    ;add esp, 4
-    ;operand_stack: %1num dd 0
-
-
-    ;Testing the allocated memory:
-    ;mov dword [eax+STK_UNIT*3], 12               ; STK_UNIT=4, the size of every link in the operand-stack.
-    ;mov ebx, dword [eax+STK_UNIT*3]
-    ;my_printf2 ebx, "The number is: %ld"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+;    cmp al, 57        
+;    jg lexical_commands
+;    cmp al, 48
+;    jl mathematical_commands
+;        numbers:
+;        sub al, 48
+;        my_printf2	eax, "The number is: %d"
+;        inc ebx                                     
+;        mov al, byte [buffer + ebx]                 
+;        cmp al, 10                                  
+;        jnz numbers                               
+;jmp loop
 
 
 
