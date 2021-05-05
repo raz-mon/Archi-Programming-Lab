d422eeaada37d8830273ddd32a99336ec3546024
razmon <razmon@post.bgu.ac.il>
2021-05-05 13:22:31 +0300

    dgh

--- Assignments/Ass_2/calc.s
+++ Assignments/Ass_2/calc.s
@@ -37,7 +37,6 @@ section	.text
 section .bss                ; Uninitialized data.
     operand_stack: resd 63                              ; Remember to save what is the size is from the user.
 section .data               ; Initialized data.
-    pointer_to_number_string: db "3"
     
 
 section .rodata             ; Read-only data.
@@ -74,42 +73,55 @@ main:
 
 
 mycalc:
-    startFunc 5                 ; Macro code will replace this. Defaultly allocetes 5*5 array (5 entrences, each of size 5).
-    mov ecx, esp                ; 
-    add ecx, 25                 ; ecx will point at the first cell in the operand stack.
+    startFunc 0                 ; Macro code will replace this. Defaultly allocetes 5*5 array (5 entrences, each of size 5).
 
     my_printf1 "calc:"           ; print "calc:".
 
-    ; ecx -> pointer to the input array of numbers (saved in ascii).
+section .data               ; Initialized data.
+    pointer_to_number_string: db "3"
+    ;len: equ $-pointer_to_number_string
+section .bss
+    first_char: resb 1
+section .text
+
+; ecx -> pointer to the input array of numbers (saved in ascii).
     mov ebx, 0                   ; Initialize bx with 0.
     mov eax, 0                   ; Initialize ax with 0.
     mov cl, 0                   ; cl will be the index counter of ax.(It is the only one that shl works with..).
     mov ecx, 0
     ; Start loop that "isolates" the relevant 8 bits.
-
-    ; Just for the test, we initialize ecx to point at "3".
     
+    ; Just for the test, we initialize ecx to point at "346542165432".
+    
+mov dl, byte [pointer_to_number_string]
+mov byte [first_char], dl
 mov edx, pointer_to_number_string
 
-bit_loop:
-    mov bl, [edx]
-    inc edx             
-
-    cmp bl, 0                   ; Check if there are any numbers left to read in the input.
-    jz print_eax_test           ; If not, finish the loop.    
-
+mov bl, byte [edx]
+add bl, 0                      ; bl will point to the end of the input number. 
+bit_loop:            
+    
     sub bl, 48                 ; get number-value of the input char (binary representation).
 
     shl bl, cl                 ; Put the bits in the right place before adding to ax.
     add cl, 3
-    add eax, ebx                 ; Add bits to the representation.
+    add ax, bx                 ; Add bits to the representation.
+
+    dec edx
+    
+    cmp bl, [first_char]        ; Check if there are any numbers left to read in the input.
+    jz print_eax_test           ; If not, finish the loop. 
+
+    mov bl, byte [edx]         ; bl points to the current character in the input.
 
     cmp cl, 8                  ; Check if we have 8 bits already.
     jl bit_loop                ; If cl<8 -> do the loop again.
 
+    
+
     ; Test: print the number we got in ax.
 print_eax_test:
-    push eax
+    push ax
     push temp2
     call printf
     add esp, 12
