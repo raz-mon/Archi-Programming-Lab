d3461221fbbcd09e6f5227e207aaee0b74dc35af
almog <almogze7@gmail.com>
2021-05-24 22:14:29 +0300

    almog

--- Labs/Lab7/almog/task 4/digit_cnt.c
+++ Labs/Lab7/almog/task 4/digit_cnt.c
@@ -2,15 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
+int digit_cnt(char * argv);
+
 int main(int argc, char * argv[]){
-    int counter = 0;
-    //if(argc > 1){
-    while (argv[1] != '\0')
-    {
-        if(strcmp(argv[1]))
-    }
-    
-    printf("%d\n" , counter);
-    //}
+    printf("%d\n" , digit_cnt(argv[1]));
     return 0;
+}
+
+int digit_cnt(char * argv){
+    int counter = 0;
+    //char c = numstr;
+    int i = 0;
+    while(argv[i] != 0){
+        if (argv[i] >= '0' && argv[i] <= '9')
+            counter++; 
+        i++;
+    }
+    return counter;
 }
